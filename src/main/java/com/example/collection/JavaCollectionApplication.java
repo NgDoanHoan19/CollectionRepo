4a6a373f16aeaa7ad884d4daa29b95c7ae30d0a6
Nguyen Doan Hoan <nguyenhoan1903@gmail.com>
2023-05-09 15:03:55 +0700

    Initial commit

---
+++ src/main/java/com/example/collection/JavaCollectionApplication.java
@@ -0,0 +1,14 @@
+package com.example.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCollectionApplication.class, args);
+        System.out.println("Chào mừng Hoan đến với Collection Java!");
+    }
+
+}
