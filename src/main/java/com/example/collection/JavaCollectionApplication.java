ed87c7289bc318e6c99ea08bb6dccd8beae9a7ae
Nguyen Doan Hoan <nguyenhoan1903@gmail.com>
2023-08-09 17:53:25 +0700

    Format code sprint-2

--- src/main/java/com/example/collection/JavaCollectionApplication.java
+++ src/main/java/com/example/collection/JavaCollectionApplication.java
@@ -3,11 +3,7 @@ package com.example.collection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.*;
-import java.util.function.BiPredicate;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.util.Collections;
 
 @SpringBootApplication
 public class JavaCollectionApplication {
